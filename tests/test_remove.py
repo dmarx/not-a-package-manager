650ffdcf5e243ced33e156da897f79051fb8e3ff
David Marx <david.marx84@gmail.com>
2022-07-15 10:37:53 -0700

    added package removal utilities and draft tests

---
+++ tests/test_remove.py
@@ -0,0 +1,13 @@
+import pytest
+import loguru
+
+from napm.remove import (
+    remove_package,
+    remove_env,
+)
+
+def test_remove_package():
+    assert True
+
+def test_remove_env():
+    assert True
